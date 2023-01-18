ef7aa0fe81c67639657c45556a7aa8a05c17b239
alejandropsan <alejandropsan1@gmail.com>
2023-01-18 21:21:43 +0100

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -20,18 +20,15 @@ CONSTRAINT pk_users PRIMARY KEY(id)
 
 CREATE TABLE IF NOT EXISTS partners(
 id              int auto_increment not null,
-user_id         int not null,
-image_user_id   int not null,
 name            varchar(50),
 company         varchar(100),
 address         varchar(100),
 image           varchar(255),
 type            varchar(30),
-amount          int,
+amount          money,
 created_at      datetime,
 updated_at      datetime,
-CONSTRAINT pk_partners PRIMARY KEY(id),
-CONSTRAINT fk_partners_users FOREIGN KEY(user_id) REFERENCES users(id)
+CONSTRAINT pk_partners PRIMARY KEY(id)
 )ENGINE="InnoDb";
 
 CREATE TABLE IF NOT EXISTS animals(
@@ -39,23 +36,23 @@ id          int auto_increment not null,
 type_id     int not null,
 name        varchar(50),
 age         tinyint,
-chip        int
-description varchar(255)
-image       varchar(255)
+chip        int,
+description varchar(255),
+image       varchar(255),
+amount      money,
 created_at  datetime,
 updated_at  datetime,
-CONSTRAINT pk_animals PRIMARY KEY(id)
+CONSTRAINT pk_animals PRIMARY KEY(id),
+CONSTRAINT fk_animals_type FOREIGN KEY animal_type(id)
 )ENGINE="InnoDb";
 
 CREATE TABLE IF NOT EXISTS animal_type(
 id          int auto_increment not null,
 animal_id   int,
-name        varchar(50),
+color        varchar(50),
 type        varchar(50),
 race        varchar(50),
-origin      varchar(50),
 country     varchar(50),
-image       varchar(255),
 created_at  datetime,
 updated_at  datetime,
 CONSTRAINT pk_animal_type PRIMARY KEY(id),
@@ -66,24 +63,22 @@ CREATE TABLE IF NOT EXISTS adopters(
 id              int auto_increment not null,
 user_id         int not null,
 animal_id       int not null,
-image_user_id   int not null,
 role_user       varchar(30),
 description     varchar(255),
 dni             int,
 image           varchar(255),
-amount          int,
+amount          money,
 created_at      datetime,
 updated_at      datetime,
 CONSTRAINT pk_adopters(id) PRIMARY KEY(id),
 CONSTRAINT fk_adopters_users FOREIGN KEY(user_id) REFERENCES users(id),
 CONSTRAINT fk_adopters_animals FOREIGN KEY(animal_id) REFERENCES animals(id),
-CONSTRAINT fk_adopters_image_users FOREIGN KEY(image_user_id) REFERENCES users(image)
+CONSTRAINT fk_adopters_role FOREIGN KEY(role_user) REFERENCES users(role)
 )ENGINE="InnoDb";
 
 CREATE TABLE IF NOT EXISTS foster_house(
 id                  int auto_increment not null,
 adopter_id          int,
-image_adopter_id    int,
 animal_id           int,
 donation_id         int,
 name                varchar(50),
@@ -91,18 +86,18 @@ address             varchar(100),
 country             varchar(50),
 description         varchar(255),
 image               varchar(255),
+amount              money,
 created_at          datetime,
 updated_at          datetime,
 CONSTRAINT pk_foster_house PRIMARY KEY(id),
 CONSTRAINT fk_foster_house_adopters FOREIGN KEY(adopter_id) REFERENCES adopters(id),
-CONSTRAINT fk_foster_house_image_adopters FOREIGN KEY(image_adopter_id) REFERENCES adopters(image),
 CONSTRAINT fk_foster_house_animals FOREIGN KEY(animal_id) REFERENCES animals(id),
 CONSTRAINT fk_foster_house_donations FOREIGN KEY(donation_id) REFERENCES donations(id)
 )ENGINE="InnoDb";
 
 CREATE TABLE IF NOT EXISTS notices(
 id          int auto_increment not null,
-animal_id  int,
+animal_id   int,
 date        date,
 location    varchar(50),
 description varchar(255),
@@ -121,7 +116,7 @@ user_id        int,
 partner_id     int,
 adopter_id     int,
 animal_id      int,
-amount         int,
+amount         money,
 created_at     datetime,
 updated_at     datetime,
 CONSTRAINT pk_donations PRIMARY KEY(id),
@@ -134,9 +129,7 @@ CONSTRAINT fk_donations_animals FOREIGN KEY(animal_id) REFERENCES animals(id)
 CREATE TABLE IF NOT EXISTS blog(
 id                  int auto_increment not null,
 user_id             int,
-image_user_id       int,
 partner_id          int,
-image_partner_id    int,
 adopter_id          int,
 foster_house_id     int,
 date_pub            date,
@@ -146,5 +139,45 @@ created_at          datetime,
 updated_at          datetime,
 CONSTRAINT pk_blog PRIMARY KEY(id),
 CONSTRAINT fk_blog_users FOREIGN KEY(user_id) REFERENCES users(id),
-CONSTRAINT fk_blog_image_users FOREIGN KEY(image_user_id) REFERENCES users(image)
+CONSTRAINT fk_blog_partners FOREIGN KEY(partner_id) REFERENCES partners(id),
+CONSTRAINT fk_blog_adopters FOREIGN KEY(adopter_id) REFERENCES adopters(id),
+CONSTRAINT fk_blog_foster_house FOREIGN KEY(foster_house_id) REFERENCES foster_house(id)
+)ENGINE="InnoDb";
+
+CREATE TABLE IF NOT EXISTS images(
+id                  int auto_increment not null,
+user_id             int,
+partner_id          int,
+adopter_id          int,
+foster_house_id     int,
+blog_id             int,
+notice_id           int,
+animal_id           int,
+image_path          varchar(255),
+description         text,
+created_at          datetime,
+updated_at          datetime,
+CONSTRAINT pk_images PRIMARY KEY(id),
+CONSTRAINT fk_images_users FOREIGN KEY(user_id) REFERENCES users(id),
+CONSTRAINT fk_images_partners FOREIGN KEY(partner_id) REFERENCES partners(id),
+CONSTRAINT fk_images_adopters FOREIGN KEY(adopter_id) REFERENCES adopters(id),
+CONSTRAINT fk_images_foster_house FOREIGN KEY(foster_house_id) REFERENCES foster_house(id),
+CONSTRAINT fk_images_blog FOREIGN KEY(blog_id) REFERENCES blog(id),
+CONSTRAINT fk_images_notices FOREIGN KEY(notice_id) REFERENCES notices(id),
+CONSTRAINT fk_images_animals FOREIGN KEY(animal_id) REFERENCES animals(id)
+)ENGINE="InnoDb";
+
+CREATE TABLE IF NOT EXISTS accounts(
+id                  int auto_increment not null,
+partner_id          int,
+adopter_id          int,
+foster_house_id     int,
+animal_id           int,
+payments            money,
+incomes             money,
+transfers           money,
+created_at          datetime,
+updated_at          datetime,
+CONSTRAINT pk_accounts PRIMARY KEY(id),
+CONSTRAINT fk_accounts_partners FOREIGN KEY(partner_id) REFERENCES partners()
 )ENGINE="InnoDb";
